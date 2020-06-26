744f4811df7bcd6971e31a45c8b59ef152a2b992
Darko Donnie <donni3thedarko@gmail.com>
2020-06-26 21:01:31 +0000

    changed logig, still does not work

--- array.h
+++ array.h
@@ -1,245 +1,131 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-
-typedef struct  {
-
-	int *arr;
-	int size;
-	int num_elements;
-
+typedef struct {
+    int* arr; 
+    int size;
+    int num_elements;
 } Array;
 
-// Create list
-Array* create_array();
-// Initialize list
-void init_array(Array *a, int element, int num_elements);
-// Add more space
-void increase_array(Array *a, int size);
-// Add element to the end of the array
-void push_array (Array* a, int element);
-// Print array
-void print_array(Array* a);
-// Sort array asc
-void sort_asc_array(Array *a);
-// Sort array des
-void sort_desc_array(Array *a);
-// update ?
-void update_array(Array *a, int pos, int element);
-// Insert element
-void insert_element(Array *a, int pos, int element);
-// Remove element
-void remove_element(Array *a, int pos, int element);
-// Search an element
-int search_element(Array *a, int element);
+Array* create_array(); // create 
+void insert(Array* a, int position, int element); // insert
+void push(Array* a, int element); // push end
+void pop(Array* a); // remove end
+void remove_elem(Array* a, int position); // remove
+int array_count(Array* a, int element); // search
+void update(Array* a, int position, int element); // update
+void sort_array(Array* a); // sort
+void print_array(Array* a); // print
+void increase_array(Array* a); // increase
+int pick(Array* a, int element); // pick
 
-
-/**************************************************/
-
-
-// Create list
-Array* create_array() {
-
-  Array* a = (Array*) malloc(sizeof(Array));
-  a->size = 0;
-  a->num_elements = 0;
-  a->arr = (int*) malloc(4);
-
-  return a;
+Array* create_array(){
+	Array* a = (Array*) malloc(sizeof(Array));
+	a->arr = (int*) calloc(10, sizeof(int));
+	a->size = 10;
+	a->num_elements = 0;
+	
+	return a;
 }
 
-// Initialize list
-void init_array(Array *a, int element, int num_elements) {
-  int i = 0;
-  a->size = num_elements;
-  a->num_elements = num_elements;
-  free(a->arr);
-
-  if(num_elements == 0) {
-
-    a->arr = (int*) calloc(1,4);
-  }
-
-  else 
-
-    a->arr = (int*) calloc(num_elements, 4);
-
-  for(i = 0; i < num_elements; ++i) {
-    *(a->arr) = element;
-    ++(a->arr);
-  }
-
-  (a->arr) -= i;
-
-}
-
-// Add more space
-void increase_array(Array *a, int size) {
-  free(a->arr);
-  a->size = size+1;
-  a->arr = (int*) calloc(a->size, 4);
-
-}
-
-// Add element to the end of the array
-void push_array (Array* a, int element) {
-  if(a->size == 0) {
-    ++a->size;
-    ++a->num_elements;
-    *(a->arr) = element;
-  }
-   else {
-     int s = a->size, i;
-     int A[s+1];
-
-     for(i = 0; i < s; ++i) {
-       A[i] = *(a->arr);
-       ++(a->arr);
-     }
-     (a->arr) -= i;
-     A[s] = element;
-
-     increase_array(a, s);
-
-     for(i = 0; i < s; ++i){
-       *(a->arr) = A[i];
-       ++(a->arr);
-     }
-      (a->arr) -= i;
-		  a->num_elements = i;
-   } // end of else
-}
-
-// Print array
-void print_array(Array* a) {
-  int i, j = 0;
-  printf(" Array { ");
-  for(i = 0; i < a->size; i++) {
-    if(i == a->size+1) {
-      printf("%i} ", *(a->arr));
-      ++j;
-    }
-    else {
-      printf("%i, ", *(a->arr));
-    }
-    ++(a->arr);
-
-  }
-  (a->arr) -= i;
-	if(j == 0){
-		printf("}");
-  }
-  printf("\n");
-}
-
-// Sort array asc
-void sort_asc_array(Array *a){
-  int i, j, k = 0;
-  for(i = 0; i < a->size; ++i) {
-    for(j = 0; j < a->size; ++i){
-      if(a->arr[i] < a->arr[j]) {
-        k = a->arr[i];
-        a->arr[i] = a->arr[j];
-        a->arr[j] = k;
-      }
-    }
-  }
-
-}
-
-// Sort array des
-void sort_desc_array(Array *a) {
-   int i, j, k = 0;
-  for(i = 0; i < a->size; ++i) {
-    for(j = 0; j < a->size; ++i){
-      if(a->arr[i] > a->arr[j]) {
-        k = a->arr[i];
-        a->arr[i] = a->arr[j];
-        a->arr[j] = k;
-      }
-    }
-  }
-}
-
-// update ?
-void update_array(Array *a, int pos, int element) {
-  if(a->size != 0 && pos <= a->size && pos > 0)
-		a->arr[pos-1] = element;
-}
-
-// Insert element
-void insert_element(Array *a, int pos, int element) {
-
-  if(pos <= a->size || a->size != 0){
-		int s = a->size, i, j, k = 0;
-		int A[s-1], AA[s];
-
-		for(i = 0; i < s; ++i){
-			AA[i] = a->arr[i];
+void insert(Array* a, int position, int element){
+	if(position > a->size)
+		printf("\n not valid position!");
+	else
+		if(a->arr[position] != NULL)
+			printf("\n not empty position");
+		else {
+			a->arr[position] = element;
+			a->num_elements++;
 		}
-
-		for(i = 0, j = 0; i < s; ++i){
-			if(i == (pos-1) && a->arr[i] == element)
-				++k;
-			else{
-				A[j] = a->arr[i];
-				++j;
-			}
-		}
-		if(k == 1){
-			increase_array(a, s-2);
-			for(i = 0; i < a->size; ++i){
-				*(a->arr) = A[i];
-				++(a->arr);
-			}
-
-			(a->arr) -= i;
-			a->num_elements = i;
-		}
-
-	}
-
 }
 
-// Remove element
-void remove_element(Array *a, int pos, int element) {
+void push(Array* a, int element){
+	if(a->num_elements == a->size)
+		increase_array(a);
+	
+	a->arr[a->num_elements] = element;
+	a->num_elements++;	
+}
 
-  if(pos <= a->size || a->size != 0){
-		int s = a->size, i, j, k = 0;
-		int A[s-1], AA[s];
+void pop(Array* a){
+	a->arr[a->num_elements] = NULL;
+	a->num_elements--;
+}
 
-		for(i = 0; i < s; ++i){
-			AA[i] = a->arr[i];
-		}
-
-		for(i = 0, j = 0; i < s; ++i){
-			if(i == (pos-1) && a->arr[i] == element)
-				++k;
-			else{
-				A[j] = a->arr[i];
-				++j;
-			}
-		}
-		if(k == 1){
-			increase_array(a, s-2);
-			for(i = 0; i < a->size; ++i){
-				*(a->arr) = A[i];
-				++(a->arr);
-			}
-
-			(a->arr) -= i;
-			a->num_elements = i;
-		}
-
+void remove_elem(Array* a, int position){
+	if(position > a->size)
+		printf("\n not valid position!");
+	else {
+		a->arr[position] = NULL;
+		a->num_elements--;
 	}
 }
 
-// Search an element
-int search_element(Array *a, int element) {
-  int i, j, k = 0;
-  for(i = 0; i < a->size; ++i) {
-    if(a->arr[i] == element)
-      return i;
-  } 
-  return -1;
+int array_count(Array* a, int element){
+	int temp = 0, i;
+	
+	for(i = 0; i < a->size; i++)
+		if(a->arr[i] == element)
+			temp++;
+	
+	return temp;
+}
+
+int pick(Array* a, int element){
+	int pos = 0, i;
+	
+	for(i = 0; i < a->element; i++)
+		if(a->arr[i] == element)
+			pos = i;
+	
+	return pos;
+}
+
+void update(Array* a, int position, int element){
+	if(a->arr[position] == NULL)
+		printf("\n empty position");
+	else {
+		a->arr[position] = element;
+		a->num_elements++;
+	}
+}
+
+void print_array(Array* a){
+	int i;
+	
+	printf("\n Array: ");
+	for(i = 0; i < a->size; i++){
+		printf("%d ", a->arr[i]);
+	}
+}
+
+void sort_array(Array* a){
+	int i, j;
+	for(i = 1; i < a->size; i++){
+        int element = a->arr[i];
+        j = i - 1;
+        while(j >= 0 && element < a->arr[j]){
+            a->arr[j+1] = a->arr[j];
+            j--;
+        }
+        a->arr[j+1] = element;
+    }
+}
+
+void increase_array(Array* a){
+	int i, temp;
+	temp = a->size;
+	
+	int* aa = (int*) calloc(a->size, sizeof(int));
+	a->size += 10;
+	
+	for(i = 0; i < temp; i++)
+		aa[i] = a->arr[i]; 
+	free(a->arr);
+	
+	a->arr = (int*) calloc(a->size, sizeof(int));
+	for(i = 0; i < temp; i++)
+		a->arr[i] = aa[i]; 
+	free(aa);	
 }
--- main.c
+++ main.c
@@ -3,27 +3,33 @@
 
 int main(void) {
   Array* new_array = create_array();
-  init_array(new_array, 3, 10);
   print_array(new_array);
   printf("\n");
-  push_array(new_array, 5);
+  push(new_array, 5);
   print_array(new_array);
   printf("\n");
-  insert_element(new_array, 0, 7);
+  insert(new_array, 0, 7);
   print_array(new_array);
   printf("\n");
-  remove_element(new_array, 0, 7);
+  pop(new_array);
   print_array(new_array);
   printf("\n");
-  update_array(new_array, 10, 9);
+  remove_elem(new_array, 0);
   print_array(new_array);
   printf("\n");
-  sort_asc_array(new_array);
+  update(new_array, 10, 9);
   print_array(new_array);
   printf("\n");
-  sort_desc_array(new_array);
+  sort_array(new_array);
   print_array(new_array);
   printf("\n");
+  int c = array_count(new_array, 11);
+  print_array(new_array);
+  printf("\n");
+  int first = pick(new_array, 11);
+  print_array(new_array);
+  printf("\n");
+
 
 
   printf("Hello World\n");
