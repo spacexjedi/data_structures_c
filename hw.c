36caf9a20b00de7fdc5830319c0ea16859dae63d
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-10-11 15:46:51 -0300

    Update hw.c

--- hw.c
+++ hw.c
@@ -2,6 +2,15 @@
 
 // Hardy-Weingberg Principle
 
+void next_generation(float a1a1, float a2a2, float a1a2) {
+    
+    a1a1 = p * p;
+    a1a2 = 2 * p * q;
+    a2a2 = q * q;
+    
+    printf("generation 1 %s %s %s", a1a1, a2a2, a1a2);
+}
+
 int main(){
 
     float a1a1 = 0.15;
@@ -11,6 +20,15 @@ int main(){
 
     float p = a1a1 + (a2a2/2);
     float q = 1- p;
+    
+     printf("generation 0 %s %s %s", a1a1, a2a2, a1a2);
+    
+    // calculing the next generation 
+    
+    next_generation(float a1a1, a2a2, a1a2);
+    
+    int number_iterations;
+
 
 
     return 0;
