2259aa1782830508d1f12acdb425fef036e49d42
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-10-13 10:33:50 -0300

    Create park.c

---
+++ park.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct dinossaur {
+    
+    char name;
+    char type;
+    int area_size;
+    char area_type;
+    char medicine;
+    int qtd;
+    
+}
+
+int main() {
+    printf("Welcome to Jurassic Park");
+    return 0;
+}
